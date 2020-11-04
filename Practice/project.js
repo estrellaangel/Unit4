b7154feec32fd4c955fe9c06a32cae7ae01436b3
Estrella <ella34138@gmail.com>
2020-11-04 15:24:35 -0700

    More Practice

--- Practice/project.js
+++ Practice/project.js
@@ -52,16 +52,68 @@
 
     // console.log(output);
 
-//FIND EVERY PRIME NUMBER FROM 1 TO 100 
+//FIND EVERY PRIME NUMBER FROM 1 TO 100
 
-    let num = 100;
+    // let string = [];
 
-    main:
-    for(let i = 2 ; i < num ; i++){
-        if(num%i == 0){
-            continue main;
-        }else{
-            console.log(i)
+    // for(let number = 1; number < 100; number++){
+    //     if(number ==1){
+    //         string += "1, ";
+    //         continue;
+    //     }else if(number ==2){
+    //         string += "2, ";
+    //         continue;
+    //     }
+    //     for(i =2; i<number; i++){
+    //         if(i == (number -1) || number == 1 || number == 2){
+    //             string+= number + ", ";
+    //             break;
+    //         }else if(number% i == 0){
+    //             break;
+    //         }
+    //     }
+    // };
+
+    // console.log(string);
+
+//LOOP THAT FINDS FACTORAL OF A NUMBER THAT IS INUPT
+
+
+    // let input = 5;
+    
+    // for(let i = (input -1); i>0; i--){
+    //     input*= i;
+    // };
+
+    // console.log(input);
+
+// Create a loop to find the greatest common factor of any 2 input numbers
+
+    // let input1 = 18;
+    // let array = 0;
+    // let input2 = 24;
+
+    // for(let i = 1; i<input1; i++){
+    //     if(input1 % i == 0){
+    //         if(input2 % i == 0){
+    //             array = i;
+    //         }
+    //     };
+    // };
+
+    // console.log(array);
+
+//CREATE A LOOP THAT WILL CREATE A DIAMOND THE SIZE OF X 
+
+ //make a loop that will start at 1 and repeat until the i gets to 5 then the other loop will start and subtract from i to get back to 0
+
+    let input = 5;
+    let i = 1;
+    let string = [];
+
+    for(; i < 5; i++){
+        for(let j = 0; j = i; j++){
+            string.push("*");
         }
-    }
-
+        console.log(string);
+    };
