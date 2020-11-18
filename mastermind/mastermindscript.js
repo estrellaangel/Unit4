f6c3d97825d7c680dfa2cdde5e9cc4a92e54996a
Estrella Angel <ella34138@gmail.com>
2020-11-18 15:54:22 -0700

    Almost finsihed CSS and started JS on Mastermind, day2

---
+++ mastermind/mastermindscript.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+let turn = 1;
+let answer = [];
+
+document.getElementById("submitButton").onclick = function(){
+    submitGuess();
+}
+
+document.getElementById("clearButton").onclick = function(){
+    clearGuess();
+}
+
+function submitGuess(){
+
+}
+
+function clearGuess(){
+
+}
